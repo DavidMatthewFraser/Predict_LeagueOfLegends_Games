d51d3e731e77aeed70a63ca337b3def61b076dd1
David Fraser <47202564+DavidMatthewFraser@users.noreply.github.com>
2021-03-22 13:22:22 -0600

    implemented player.getRank

--- app/Player.py
+++ app/Player.py
@@ -6,7 +6,7 @@ class Player:
         self.id = reader.get_SUMMONER_V4(self.name, apiKey.key)['id'] 
     def getMastery(self, championId):
         print(reader.get_CHAMPION_MASTERY_V4(self.id, championId, apiKey.key)['championPoints'])
-    def getRank():
-        print('rank')
+    def getRank(self):
+        print(reader.get_SUMMONER_LEAGUE_V4(self.id, apiKey.key))
     def getWinrate():
         print('winrate')
