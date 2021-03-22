43d942c7754e8adb124856f0ceecc4bbf2ad48d6
David Fraser <47202564+DavidMatthewFraser@users.noreply.github.com>
2021-03-22 13:21:45 -0600

    implemented player.getRank

--- app/ApiReader.py
+++ app/ApiReader.py
@@ -12,6 +12,20 @@ class ApiReader:
     def get_LEAGUE_V4(tier, division, page, api_key):
         LEAGUE_V4 = 'https://na1.api.riotgames.com/lol/league/v4/entries/RANKED_SOLO_5x5/'
         return requests.get(LEAGUE_V4 + tier + "/" + division + "?page=" + str(page) + "&api_key=" + api_key).json()
+    
+    def get_SUMMONER_LEAGUE_V4(summoner_id, api_key):
+        LEAGUE_V4 = 'https://na1.api.riotgames.com/lol/league/v4/entries/by-summoner/'
+        response = requests.get(LEAGUE_V4 + summoner_id + "?api_key=" + api_key).json()
+        tiers = {'DIAMOND' : 6, 'PLATINUM': 5, 'GOLD': 4, 'SILVER': 3, 'BRONZE': 2, 'IRON': 1}
+        divisions = {'I': (4/5), 'II': (3/5), 'III': (2/5), 'IV': (1/5)}
+        rank = 0
+        for queueType in response:
+            tier = queueType['tier']
+            division = queueType['rank']
+            lp = queueType['leaguePoints']
+            rank = max(rank, tiers[tier] + divisions[division] + (lp * (1/5) / 100))
+        return rank
+
 
     def get_MATCHES_V4(id, api_key):
         MATCHES_V4 = 'https://na1.api.riotgames.com/lol/match/v4/matchlists/by-account/'
