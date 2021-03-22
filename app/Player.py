d492868ec3c912f00183b6ca294862a9451f9f4e
David Fraser <47202564+DavidMatthewFraser@users.noreply.github.com>
2021-03-22 13:26:24 -0600

    calculated rank now done in Player not ApiReader

--- app/Player.py
+++ app/Player.py
@@ -7,6 +7,15 @@ class Player:
     def getMastery(self, championId):
         print(reader.get_CHAMPION_MASTERY_V4(self.id, championId, apiKey.key)['championPoints'])
     def getRank(self):
-        print(reader.get_SUMMONER_LEAGUE_V4(self.id, apiKey.key))
+        response = reader.get_SUMMONER_LEAGUE_V4(self.id, apiKey.key)
+        tiers = {'DIAMOND' : 6, 'PLATINUM': 5, 'GOLD': 4, 'SILVER': 3, 'BRONZE': 2, 'IRON': 1}
+        divisions = {'I': (4/5), 'II': (3/5), 'III': (2/5), 'IV': (1/5)}
+        rank = 0
+        for queueType in response:
+            tier = queueType['tier']
+            division = queueType['rank']
+            lp = queueType['leaguePoints']
+            rank = max(rank, tiers[tier] + divisions[division] + (lp * (1/5) / 100))
+        print(rank)
     def getWinrate():
         print('winrate')
